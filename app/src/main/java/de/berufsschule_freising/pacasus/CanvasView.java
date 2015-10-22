ab05c82cea466b7de76622156b44786ee1a270e5
Gabsch <gabriel.d.schneider@outlook.com>
2015-10-22 12:00:40 +0200

    Implemented Canvas

---
+++ app/src/main/java/de/berufsschule_freising/pacasus/CanvasView.java
@@ -0,0 +1,34 @@
+package de.berufsschule_freising.pacasus;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.view.View;
+
+/**
+ * Created by Gabriel on 22.10.2015.
+ */
+public class CanvasView extends View{
+
+	public CanvasView(Context context)
+	{
+		super(context);
+		//TODO constructor
+	}
+
+	@Override
+	protected void onDraw(Canvas canvas) {
+		super.onDraw(canvas);
+		int height = getHeight();
+		int width = getWidth();
+		int radius = 100;
+		Paint paint = new Paint();
+		paint.setStyle(Paint.Style.FILL);
+		paint.setColor(Color.BLACK);
+		canvas.drawPaint(paint);
+		paint.setColor(Color.BLUE);
+		canvas.drawCircle(height / 2, width / 2, radius, paint);
+
+	}
+}
